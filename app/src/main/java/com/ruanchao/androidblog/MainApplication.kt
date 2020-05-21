3c6744b88e8e325e5ec249dc3c67d49b0b3134bc
阮超 <ruanchao@shuwen.com>
2020-05-21 19:33:17 +0800

    Fix:test2

--- app/src/main/java/com/ruanchao/androidblog/MainApplication.kt
+++ app/src/main/java/com/ruanchao/androidblog/MainApplication.kt
@@ -16,8 +16,6 @@ class MainApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         Log.i("MainApplication","onCreate")
-
-        
         context = this
     }
 
